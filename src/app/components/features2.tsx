3394e3e748253439e71aba03b675730b9864bd6a
AhmedSaeed4 <160440087+AhmedSaeed4@users.noreply.github.com>
2024-12-17 00:35:51 +0500

    Fixed unescaped entities and finalized build

--- src/app/components/features2.tsx
+++ src/app/components/features2.tsx
@@ -28,9 +28,9 @@ const Features2 = () => {
     <div className="sm:w-[1440px] sm:h-[355px] w-[390px] bg- sm:px-[86px] sm:pb-[86px] pb-[45px]  px-[24px] pt-[60px] flex flex-col sm:items-center  sm:gap-[51px] gap-[36px]">
         <p className="font-clash font-normal sm:text-[24px] text-[20px] sm:leading-[33.6px] leading-[28px] pr-[68px] sm:pr-0 ">What makes our brand different</p>
         <div className=" grid sm:grid-cols-4 sm:gap-[59px] gap-[36px] grid-cols-1 ">
-            {data.map((elem)=>{
+            {data.map((elem, index)=>{
                 return(
-                    <Card2 image={elem.image} title={elem.title} content={elem.content} />
+                    <Card2 key={index} image={elem.image} title={elem.title} content={elem.content} />
                 )
             })}
         </div>
